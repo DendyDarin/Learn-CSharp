64ea4d02c9ea7e4288bc3abdb4a7cac188a3ceea
Dendy Darin <dendy050677@gmail.com>
2025-08-22 02:35:10 +0900

    Do while loop and nested loop

---
+++ ConsoleApp.Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿// Example - Print Hello World 10 times
+
+//Console.WriteLine("Hello World");
+
+//Console.Write("How many times you want to print that word? ");
+
+//int answer = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ok, FOR LOOP result:");
+
+//for (int i = 0; i < answer; i++)
+//{
+//    Console.WriteLine("Hello World");
+//}
+
+// WHILE LOOP
+//Console.WriteLine();
+//Console.WriteLine("WHILE LOOP result:");
+
+//int counter = 0;
+
+//while (counter < answer)
+//{
+//    Console.WriteLine("Hello World");
+//    counter++;
+//}
+
+// EXERCISE
+// Ask the user for a number and find the total for each number. Print final sum when user enters -1 to exit.
+
+int sum = 0;
+int num = 0;
+
+while (num != -1)
+{
+    Console.Write("Angka untuk dijumlah. (Masukkan -1 untuk menghitung total penjumlahan: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num != -1)
+    {
+        sum += num;
+    }
+}
+
+Console.WriteLine($"Jumlah semuanya: {sum}");
+
+// DO WHILE LOOP
+
+Console.WriteLine("***** DO WHILE LOOP *****");
--- ConsoleApp.SimpleCalculator/Program.cs
+++ ConsoleApp.SimpleCalculator/Program.cs
@@ -1,47 +1,79 @@
 ﻿// Welcome Message
+using System.Runtime.InteropServices.Marshalling;
+
 Console.WriteLine("Welcome to simple calculator");
 Console.WriteLine();
 
 // Prompt for user input
-Console.Write("Enter first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
 
 // Show calculator option
 Console.WriteLine();
-Console.WriteLine("Operation's options:");
+Console.WriteLine("Operation's options (-1 to exit):");
 Console.WriteLine("1 - Addition");
 Console.WriteLine("2 - Subtraction");
 Console.WriteLine("3 - Multiply");
 Console.WriteLine("4 - Division");
+Console.WriteLine("5 - Fibonacci sequence");
 Console.WriteLine();
 Console.Write("What do you want to operate? ");
 
 int operation = Convert.ToInt32(Console.ReadLine());
-int add = num1 + num2;
-int sub = num1 - num2;
-int mul = num1 * num2;
-int div = num1 / num2;
 
-switch (operation)
+// check the choice first
+while (operation != -1)
 {
-    case 1:
-        Console.WriteLine($"The addition is {add}");
-        break;
-    case 2:
-        Console.WriteLine($"The subtraction is {sub}");
-        break;
-    case 3:
-        Console.WriteLine($"The multiply is {mul}");
-        break;
-    case 4:
-        Console.WriteLine($"The division is {div}");
-        break;
-    default:
-        Console.WriteLine("Invalid input operation.");
-        break;
+    // prompt user input
+    Console.Write("Enter first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    int answer = 0;
+
+    switch (operation)
+    {
+        case 1:
+            answer = num1 + num2;
+            break;
+        case 2:
+            answer = num1 - num2;
+            break;
+        case 3:
+            answer = num1 * num2;
+            break;
+        case 4:
+            answer = num1 / num2;
+            break;
+        case 5:
+            for (int i = num1; i <= num2; i++)
+            {
+                answer += i;
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid input operation.");
+            break;
+    }
+
+Console.WriteLine($"The result is {answer}");
+Console.WriteLine("Please enter to continue");
+Console.ReadLine();
+Console.Clear(); // to clear screen
+
+Console.WriteLine("Operation's options (-1 to exit):");
+Console.WriteLine("1 - Addition");
+Console.WriteLine("2 - Subtraction");
+Console.WriteLine("3 - Multiply");
+Console.WriteLine("4 - Division");
+Console.WriteLine("5 - Fibonacci sequence");
+Console.WriteLine();
+Console.Write("What do you want to operate? ");
+
+operation = Convert.ToInt32(Console.ReadLine());
 }
 
+
+Console.WriteLine();
 Console.WriteLine("Thank you using sample calculator.");
